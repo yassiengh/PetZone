e022548eab33f7272bb451860f694a8cbdbe994d
yassien ghoniem <yassieng@yahoo.com>
2022-01-19 19:57:32 +0200

    spelling mistake

--- controllers/authController.js
+++ controllers/authController.js
@@ -56,8 +56,8 @@ exports.login = catchAsync(async (req, res, next) => {
     return next(new AppError("Incorrect email or password", 401));
   }
 
-  if(req.body.verified === false) {
-    return next(new AppError("Please active your email ", 401));
+  if (req.body.verified === false) {
+    return next(new AppError("Please activate your email ", 401));
   }
 
   // 3) If everything ok, send token to client
